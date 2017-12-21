aaba60265ced1c1280b68d0e13c2d3b3eeff4ff6
Larry Li <larry.zy.li@gmail.com>
2017-12-21 13:46:38 -0600

    finished closures

--- closures.js
+++ closures.js
@@ -3,95 +3,78 @@
 \******************************************************************************/
 
 function outer() {
-    var name = 'Tyler';
+    var name = "Tyler";
     return function() {
-      return 'The original name was ' + name;
-    }
-  }
-  
-  /****** INSTRUCTIONS PROBLEM 1 ******/
-  /* Above you're given a function that returns another function which has a
+        return "The original name was " + name;
+    };
+}
+
+/****** INSTRUCTIONS PROBLEM 1 ******/
+/* Above you're given a function that returns another function which has a
   closure over the name variable. Invoke outer saving the return value into
   another variable called 'inner'. */
-  
-  // Code Here
-  
-  //Once you do that, invoke inner.
-  
-  //Code Here
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  /******************************************************************************\
+
+// Code Here
+const inner = outer();
+
+//Once you do that, invoke inner.
+
+//Code Here
+inner();
+
+/******************************************************************************\
       #PROBLEM-02
-  \******************************************************************************/
-  
-  
-  function callFriend(name) {
+\******************************************************************************/
+
+function callFriend(name) {
     function dial(number) {
-      return 'Calling ' + name + ' at ' + number
+        return "Calling " + name + " at " + number;
     }
     return dial;
-  }
-  
-  /****** INSTRUCTIONS PROBLEM 2 ******/
-  /* Above you're given a callFriend function that returns the dial function.
+}
+
+/****** INSTRUCTIONS PROBLEM 2 ******/
+/* Above you're given a callFriend function that returns the dial function.
   
   Store the result of invoking callFriend in a variable named callJake.
   
   When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248' 
   (HINT: You will need to pass in arguments to both function invocations)
   */
-  
-    //Code Here
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  /******************************************************************************\
+
+//Code Here
+const callJake = callFriend("Jake");
+callJake("435-555-9248");
+
+/******************************************************************************\
       #PROBLEM-03
-  \******************************************************************************/
-  
-  /****** INSTRUCTIONS PROBLEM 3 ******/
-  /* Write a function called makeCounter that makes the following code work
+\******************************************************************************/
+
+/****** INSTRUCTIONS PROBLEM 3 ******/
+/* Write a function called makeCounter that makes the following code work
   properly. */
-  
-  //Code Here
-  
-  //Uncomment this once you make your function
-  //   var count = makeCounter();
-  //   count(); // 1
-  //   count(); // 2
-  //   count(); // 3
-  //   count(); // 4
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  /******************************************************************************\
+
+//Code Here
+function makeCounter() {
+    let num = 0;
+    return function() {
+        return (num += 1);
+    };
+}
+
+//Uncomment this once you make your function
+//   var count = makeCounter();
+//   count(); // 1
+//   count(); // 2
+//   count(); // 3
+//   count(); // 4
+
+/******************************************************************************\
       #PROBLEM-04
-  \******************************************************************************/
-  
-  /****** INSTRUCTIONS PROBLEM 4 ******/
-  /* Inside the function called counterFactory return two functions that implement
+\******************************************************************************/
+
+/****** INSTRUCTIONS PROBLEM 4 ******/
+/* Inside the function called counterFactory return two functions that implement
   up/down counter. The first function is called inc, this function is responsible
   for incrementing the value once and returning the updated value. The second function 
   is called dec, this function is responsible for decrementing the value by one 
@@ -99,121 +82,113 @@ function outer() {
   achieve this. Information on the module pattern available here: 
   http://stackoverflow.com/questions/17776940/javascript-module-pattern-with-example?answertab=votes#tab-top
   */
-  
-  function counterFactory(value) {
-  
-    // Code here.
-  
-  
-    return {
 
-    }
-  }
-  
-  
-  counter = counterFactory(10);
-  // counter.inc() // 11
-  // counter.inc() // 12
-  // counter.inc() // 13
-  // counter.dec() // 12
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  /******************************************************************************\
+function counterFactory(value) {
+    // Code here.
+    return {
+        inc: function() {
+            return (value += 1);
+        },
+        dec: function() {
+            return (value -= 1);
+        }
+    };
+}
+
+counter = counterFactory(10);
+// counter.inc() // 11
+// counter.inc() // 12
+// counter.inc() // 13
+// counter.dec() // 12
+
+/******************************************************************************\
       #PROBLEM-05
-  \******************************************************************************/
-  
-  /****** INSTRUCTIONS PROBLEM 5 ******/
-  /* Inside the motivation function create another function called message that
+\******************************************************************************/
+
+/****** INSTRUCTIONS PROBLEM 5 ******/
+/* Inside the motivation function create another function called message that
   will return the welcome text with the firstname and lastname. The final message should
   say "You're doing awesome, keep it up firstname lastname." 
   (Hint: don't forget to have a space between the firstname and lastname and a period
   at the end of the sentence.)  */
-  
-  function motivation(firstname, lastname) {
-  
-    var welcomeText = 'You\'re doing awesome, keep it up ';
-  
+
+function motivation(firstname, lastname) {
+    var welcomeText = "You're doing awesome, keep it up ";
+
     // code message function here.
-  
-  
+    function message() {
+        return `${welcomeText}${firstname} ${lastname}.`;
+    }
+
     //Uncommment this to return the value of your message function
-    //return message;
-  
-  }
-  
-  var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  /******************************************************************************\
+    return message;
+}
+
+var greeting = motivation("Billy", "Bob"); // 'You're doing awesome keep it up Billy Bob.
+
+/******************************************************************************\
       #PROBLEM-06
-  \******************************************************************************/
-  
-  /****** INSTRUCTIONS PROBLEM 6 ******/
-  /* Inside the module's return object create a publicMethod function that
+\******************************************************************************/
+
+/****** INSTRUCTIONS PROBLEM 6 ******/
+/* Inside the module's return object create a publicMethod function that
   invokes privateMethod (return the result). Invoke this by calling module.publicMethod(); outside
   the module scope */
-  
-  var module = (function() {
+
+var module = (function() {
     var person = {
-      name: "phillip",
-      age: 29,
-      location: "Utah"
+        name: "phillip",
+        age: 29,
+        location: "Utah"
     };
-  
-    function privateMethod(){
-      return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
+
+    function privateMethod() {
+        return (
+            "Hi, I'm " +
+            person.name +
+            ", age " +
+            person.age +
+            " from " +
+            person.location
+        );
     }
-  
+
     // Anything that is being returned is made public and can be invoked from
     // outside our lexical scope
     return {
-      // Code here.
+        // Code here.
+        publicMethod: privateMethod
     };
-  
-  })();
-  
-  
-  
-  /******************************************************************************\
-   #PROBLEM-07
-   \******************************************************************************/
+})();
 
-  /****** INSTRUCTIONS PROBLEM 7 ******/
-  /* Here we have a function named secretNumber that has a secret number. Inside
+/******************************************************************************\
+   #PROBLEM-07
+\******************************************************************************/
+
+/****** INSTRUCTIONS PROBLEM 7 ******/
+/* Here we have a function named secretNumber that has a secret number. Inside
   the return object, create two methods called addToSecret and takeAwayFromSecret. addToSecret should have a parameter that is added to the secret number returning the updated secret number. takeAwayFromSecret should have a parameter that takes away from the secret number returning the updated secret number. */
 
-  function secretNumber() {
+function secretNumber() {
     var secret = 143;
 
     return {
-      // Code here
-    }
-  }
-  
-  
-  
-  /******************************************************************************\
+        // Code here
+        addToSecret: function(value) {
+            return (secret += value);
+        },
+        takeAwayFromSecret: function(value) {
+            return (secret -= value);
+        }
+    };
+}
+
+/******************************************************************************\
       #PROBLEM-08
-  \******************************************************************************/
-  
-  /****** INSTRUCTIONS PROBLEM 8 ******/
-  /* Here we have a for loop that will iterate as long as i is less than or equal
+\******************************************************************************/
+
+/****** INSTRUCTIONS PROBLEM 8 ******/
+/* Here we have a for loop that will iterate as long as i is less than or equal
   to 5. What we need to do is console.log(i) so that it logs like so:
    0 second after call - log 0
    1 seconds after call - log 1
@@ -227,13 +202,15 @@ function outer() {
   
    Fix the code below to log the desired output.
    */
-  
-  function timeOutCounter() {
+
+function timeOutCounter() {
     for (var i = 0; i <= 5; i++) {
-      setTimeout(function() {
-          console.log(i)
-      }, i * 1000)
+        function closure(index) {
+            setTimeout(function() {
+                console.log(index);
+            }, index * 1000);
+        }
+        closure(i);
     }
-  }
-  timeOutCounter();
-  
+}
+timeOutCounter();
